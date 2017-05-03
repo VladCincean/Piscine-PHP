8b6d96ffcbc2203bbf9d808779b8a1c7206511df
VladCincean <cvie1883@scs.ubbcluj.ro>
2017-05-03 22:56:32 +0300

    day07+

--- day_07/ex06/Fighter.class.php
+++ day_07/ex06/Fighter.class.php
@@ -1,3 +1,19 @@
 <?php
+abstract class Fighter {
+	private $type;
 
+	public function __construct($type) {
+		$this->type = $type;
+	}
+
+	public function getType() {
+		return ($this->type);
+	}
+
+	public function __clone() {
+		//...
+	}
+
+	public abstract function fight($with);
+}
 ?>
--- day_07/ex06/UnholyFactory.class.php
+++ day_07/ex06/UnholyFactory.class.php
@@ -1,3 +1,31 @@
 <?php
+class UnholyFactory {
+    private $array;
 
+    public function __construct() {
+    	$this->array = array();
+    }
+
+    public function absorb($f) {
+        if ($f instanceof Fighter) {
+            if (isset($this->array[$f->getType()])) {
+                echo "(Factory already absorbed a fighter of type " . $f->getType() . ")" . PHP_EOL;
+            } else {
+            	$this->array[$f->getType()] = $f;
+                echo "(Factory absorbed a fighter of type " . $f->getType() . ")" . PHP_EOL;
+            }
+        } else {
+            echo "(Factory can't absorb this, it's not a fighter)" . PHP_EOL;
+        }
+    }
+
+    public function fabricate($f) {
+        if (array_key_exists($f, $this->array)) {
+            echo "(Factory fabricates a fighter of type " . $f . ")" . PHP_EOL;
+            return clone $this->array[$f];
+        }
+        echo "(Factory hasn't absorbed any fighter of type " . $f . ")" . PHP_EOL;
+        return null;
+    }
+}
 ?>
